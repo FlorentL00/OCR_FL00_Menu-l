32da1d30aabea3c4dca14ff1b5e267605eca62e6
FlorentL00 <florent0710@gmail.com>
2018-12-27 18:01:55 +0100

    First commit to start OCR with original bases

---
+++ src/com/florentL00/florent/Order.java
@@ -0,0 +1,17 @@
+package com.florentL00.florent;
+
+public class Order {
+    /**
+     * Display all available menus in the restaurant.
+     */
+    public void displayAvailableMenu() {
+
+    }
+    /**
+     * Display a selected menu.
+     * @param nbMenu The selected menu.
+     */
+    public void displaySelectedMenu(int nbMenu) {
+
+    }
+}
