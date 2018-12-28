5cd2d731a04f8f9c2e9d75169dab39203974d88e
FlorentL00 <florent0710@gmail.com>
2018-12-28 15:17:52 +0100

    Course project part 4: Add method and behavior in order : - method displaySelectedSide add if condition and switch for side choice

--- src/com/florentL00/florent/Order.java
+++ src/com/florentL00/florent/Order.java
@@ -14,36 +14,76 @@ public class Order {
         System.out.println("Que souhaitez-vous comme menu?");
 
     }
+
     /**
      * Display a selected menu.
+     *
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-        switch (nbMenu){
-        case 1:
-            System.out.println("Vous avez choisi comme menu : poulet");
-            break;
-        case 2:
-            System.out.println("Vous avez choisi comme menu : boeuf");
-            break;
-        case 3:
-            System.out.println("Vous avez choisi comme menu : végétarien");
-            break;
-        default:
-            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
-            break;
+        switch (nbMenu) {
+            case 1:
+                System.out.println("Vous avez choisi comme menu : poulet");
+                break;
+            case 2:
+                System.out.println("Vous avez choisi comme menu : boeuf");
+                break;
+            case 3:
+                System.out.println("Vous avez choisi comme menu : végétarien");
+                break;
+            default:
+                System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+                break;
         }
     }
 
     /**
      * Run asking process for a menu.
      */
-
-    public void runMenu(){
+    public void runMenu() {
 
         this.displayAvailableMenu();
         Scanner sc = new Scanner(System.in);
         int nb = sc.nextInt();
         this.displaySelectedMenu(nb);
+    }
+
+    /**
+     * Display a selected side depending on all sides enable or not.
+     * All sides = vegetables, fries and rice.
+     * Not all sides = rice or not.
+     *
+     * @param nbSide         the selected side.
+     * @param allSidesEnable enable display for all sides or not.
+     */
+    public void displaySelectedSide(int nbSide, boolean allSidesEnable) {
+        if (allSidesEnable) {
+            switch (nbSide) {
+                case 1:
+                    System.out.println("Vous avez choisi comme accompagnement : légumes frais");
+                    break;
+                case 2:
+                    System.out.println("Vous avez choisi comme accompagnement : frites");
+                    break;
+                case 3:
+                    System.out.println("Vous avez choisi comme accompagnement : riz");
+                    break;
+                default:
+                    System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+                    break;
+            }
+        } else {
+            switch (nbSide) {
+                case 1:
+                    System.out.println("Vous avez choisi comme accompagnement : riz");
+                    break;
+                case 2:
+                    System.out.println("Vous avez choisi comme accompagnement : pas de riz");
+                    break;
+                default:
+                    System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+                    break;
+            }
+        }
     }
 }
