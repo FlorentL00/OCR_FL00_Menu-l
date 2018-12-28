98063ad994822a266ad83fbfb1a03b7bd3c33e2f
FlorentL00 <florent0710@gmail.com>
2018-12-28 15:23:38 +0100

    Course project part 4: Add method and behavior in order : - method displaySelectedDrink add switch condition for drink choice

--- src/com/florentL00/florent/Order.java
+++ src/com/florentL00/florent/Order.java
@@ -52,7 +52,6 @@ public class Order {
      * Display a selected side depending on all sides enable or not.
      * All sides = vegetables, fries and rice.
      * Not all sides = rice or not.
-     *
      * @param nbSide         the selected side.
      * @param allSidesEnable enable display for all sides or not.
      */
@@ -84,6 +83,27 @@ public class Order {
                     System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
                     break;
             }
+        }
+    }
+
+    /**
+     * Display a selected drink.
+     * @param nbDrink The selected drink
+     */
+    public void displaySelectedDrink(int nbDrink) {
+        switch (nbDrink){
+            case 1:
+                System.out.println("Vous avez choisi comme boisson : eau plate");
+                break;
+            case 2:
+                System.out.println("Vous avez choisi comme boisson : eau gazeuse");
+                break;
+            case 3:
+                System.out.println("Vous avez choisi comme boisson : soda");
+                break;
+            default:
+                System.out.println("Vous n'avez pas choisi de boisson parmi les choix proposés");
+                break;
         }
     }
 }
